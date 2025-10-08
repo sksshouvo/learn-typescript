4c47c98c23cba0738ae27bed2721fcd2e317a3a9
Salman Kabir <sksshouvo2@gmail.com>
2025-10-08 13:02:51 +0600

    Learned object type aliases

--- objectTypeAlias.js
+++ objectTypeAlias.js
@@ -2,6 +2,6 @@ function createUser(user) {
     if (user.email == "sksshouvo2@gmail.com") {
         return { name: "Salman kabir", email: user.email, isActive: true };
     }
-    return user;
+    return { name: "", email: "", isActive: false };
 }
 console.log(createUser({ name: "Salman Kabir", email: "sksshouvo2@gmail.com", isActive: true }));
